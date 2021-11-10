d5c04c362d7055b537d92fcc2b2a9b76a255c8ec
91678 <916781283@qq.com>
2021-11-10 20:30:05 +0800

    完成了工位页面

--- src/view/data/Station.vue
+++ src/view/data/Station.vue
@@ -24,21 +24,49 @@ export default {
   name: 'Station',
   data () {
     return {
-      tableSearchList: [],
+      refName: 'StationForm',
+      fileName: 'Station.xlsx',
+      url: {
+        refreshUrl: '/basicCategoryData/list/station',
+        searchUrl: '/basicCategoryData/list/station',
+        addUrl: '/basicCategoryData/add',
+        editUrl: '/basicCategoryData/',
+        deleteUrl: '/basicCategoryData/deleteList',
+        uploadUrl: '/basicCategoryData/addList'
+      },
+      searchList: [
+        {value: 'name', label: '工位名称'},
+        {value: 'category', label: '所属类别'},
+        {value: 'categoryNumber', label: '类别编号'},
+        {value: 'remark', label: '备注'},
+        {value: 'createUser', label: '创建人'},
+        {value: 'createGmt', label: '创建时间'},
+        {value: 'modifiedUser', label: '修改人'},
+        {value: 'modifiedGmt', label: '修改时间'}
+      ],
       tableHeaderList: [
         {value: 'name', label: '工位名称', minWidth: 200},
         {value: 'category', label: '所属类别', minWidth: 200},
         {value: 'categoryNumber', label: '类别编号', minWidth: 200},
-        {value: 'fixed', label: '其他字段', minWidth: 200},
+        {value: 'remark', label: '备注', width: 200},
         {value: 'createUser', label: '创建人', width: 200},
         {value: 'createGmt', label: '创建时间', width: 200},
         {value: 'modifiedUser', label: '最后修改人', width: 200},
         {value: 'modifiedGmt', label: '最后修改时间', width: 200}
       ],
-      addForm: {},
+      addForm: {name: null, category: null, categoryNumber: null, remark: null},
       addFormRules: {
         name: [
-          {required: true, message: '非空验证示例!', trigger: 'blur'}
+          {required: true, message: '工位名称不能为空!', trigger: 'blur'}
+        ],
+        category: [
+          {required: true, message: '所属类别不能为空!', trigger: 'blur'}
+        ],
+        categoryNumber: [
+          {required: true, message: '类别编号不能为空!', trigger: 'blur'}
+        ],
+        remark: [
+          {required: true, message: '备注不能为空!', trigger: 'blur'}
         ]
       },
       editForm: {},
